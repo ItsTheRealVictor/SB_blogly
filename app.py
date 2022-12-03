abcef1e924313b14ac49293496123d834288184d
Victor Delgado <valexdelgado@gmail.com>
2022-12-03 00:44:40 -0800

    functionality update: now able to add users using the form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_migrate import Migrate, migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -23,7 +23,8 @@ class User(db.Model):
     
     # no idea how to do this, stackoverflow isn't showing anything compelling. I need to ask a tutor
     # In the meantime, this is a placeholder
-    image_name = db.Column(db.String(50), nullable=True, unique=False, default='Cool Image!')
+    PLACEHOLDER = 'https://www.clipartmax.com/png/middle/340-3400182_special-school-nurse-placeholder-person.png'
+    image_url = db.Column(db.String(50), nullable=True, unique=False, default=PLACEHOLDER)
     
     def __repr__(self):
         return f'User #{self.id}: Full name is {self.first_name} {self.last_name}'
@@ -34,4 +35,20 @@ class User(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    users = User.query.all()
+    return render_template('home.html', users=users)
+
+@app.route('/add_user')
+def add_form():
+    return render_template('add_user.html')
+
+@app.route('/', methods=['POST'])
+def add_user():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    image_url = request.form.get('image_url')
+    
+    user_to_add = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user_to_add)
+    db.session.commit()
+    return redirect('/')
